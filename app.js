17a4c25ea53064970f1f094a3f64605e01b96727
Amir prodhan <amir25410475@gmail.com>
2022-02-11 16:12:07 +0600

    new enter

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 
+window.onload = () => {
+    main()
+}
 
+// main function
+const main = () => {
+    let bgColor = document.getElementById('bgColor')
+    let btn = document.getElementById('btn')
 
-console.log('hello')
+    btn.addEventListener('click', () => {
+        bgColor.style.backgroundColor = bgColorGenerator()
+        bgColor.style.color = FontColorGenerator()
+    })
+}
+
+// background color change function
+const bgColorGenerator = () => {
+    let red = Math.floor(Math.random() * 255)
+    let blue = Math.floor(Math.random() * 255)
+    let green = Math.floor(Math.random() * 255)
+
+    return `rgba(${red}, ${blue}, ${green})`
+}
+
+// font color change function
+const FontColorGenerator = () => {
+    let red = Math.floor(Math.random() * 200)
+    let blue = Math.floor(Math.random() * 100)
+    let green = Math.floor(Math.random() * 300)
+
+    return `rgba(${red}, ${blue}, ${green})`
+}
+
 
